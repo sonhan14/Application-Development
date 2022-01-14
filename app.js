9657d2074fbd9e1562d3fbb2bad1f68a0ff90f2a
sonhan14 <sonhan14@gmail.com>
2022-01-14 13:07:56 +0700

    search by name revert

--- app.js
+++ app.js
@@ -1,54 +1,18 @@
 const express = require("express");
 const app = express();
 const dbHandler = require("./databaseHandler");
-const session = require('express-session');
-app.use(session({
-  //setting for session
-  resave: true,
-  saveUninitialized: true,
-  secret: 'mysecret12345@@@@@',
-  cookie: { maxAge: 24 * 60 * 60 * 1000 }
-}));
 
 app.set("view engine", "hbs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const userController = require("./controllers/customer")
+app.use("/", userController);
+
 const adminController = require("./controllers/admin");
 //cac request co chua /admin se di den controller admin
 app.use("/admin", adminController);
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
-app.get("/order", (req, res) => {
-  res.render("order");
-});
-
-app.get('/login', (req, res) => {
-  res.render('login');
-})
-
-app.post('/doLogin', async (req, res) => {
-  const nameInput = req.body.name;
-  const passInput = req.body.password;
-  const found = await dbHandler.checkUser(nameInput, passInput);
-  if (found) {
-      req.session.username = nameInput;
-      res.render('index', { loginName: nameInput })
-  } else {
-      res.render('login', { errorMsg: "Login failed!" })
-  }
-})
-
-
-app.get("/getOrder", (req, res) => {
-  req.query.time = new Date();
-  console.log(req.query);
-  dbHandler.insertObject("order", req.query);
-  res.send("Done");
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- controllers/customer.js
+++ controllers/customer.js
@@ -27,5 +27,4 @@ router.get('/',async(req,res)=>{
     }
 })
 
-
 module.exports = router;
