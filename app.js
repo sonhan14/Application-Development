dbda5119a1779601516ee21050dc553a4e22aa0a
CodeLAB <91421918+DO-HOAI-NAM@users.noreply.github.com>
2022-01-14 21:23:57 +0700

    test commit

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ app.use(express.urlencoded({ extended: true }));
 //hellotest
 app.use(express.static("public"));
 
-const userController = require("./controllers/customer")
+const userController = require("./controllers/customer");
 app.use("/", userController);
 
 const adminController = require("./controllers/admin");
 //cac request co chua /admin se di den controller admin
 app.use("/admin", adminController);
 
-
+//hello 12344
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
