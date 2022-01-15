3b4240c8aad4e966dcf0c585c91f0f79a33709e7
sonhan14 <sonhan14@gmail.com>
2022-01-15 13:08:42 +0700

    taoj request den login controller

--- app.js
+++ app.js
@@ -4,20 +4,19 @@ const dbHandler = require("./databaseHandler");
 
 app.set("view engine", "hbs");
 app.use(express.urlencoded({ extended: true }));
-//hellotest
 app.use(express.static("public"));
 
+//cac request co chua /admin se di den controller customer
 const userController = require("./controllers/customer");
 app.use("/", userController);
 
-const adminController = require("./controllers/admin");
 //cac request co chua /admin se di den controller admin
+const adminController = require("./controllers/admin");
 app.use("/admin", adminController);
 
-//hello 12344
-app.get("/login", (req,res)=>{
-    res.render('login')
-})
+//cac request co chua /admin se di den controller admin
+const loginControler = require('./controllers/login');
+app.use("/login", loginControler)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
