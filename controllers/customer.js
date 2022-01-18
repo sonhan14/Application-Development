6b4b780c383613c7eb4325779d6ef0cbea51f911
sonhan14 <sonhan14@gmail.com>
2022-01-18 21:51:06 +0700

    find way to do shopping cart

--- controllers/customer.js
+++ controllers/customer.js
@@ -3,6 +3,12 @@ const async = require('hbs/lib/async')
 const {searchObjectbyCategory, searchObjectbyPrice, searchObjectbyName, insertObject, getAll, deleteDocumentById, getDocumentById, updateDocument, } = require('../databaseHandler')
 const router = express.Router()
 
+router.get('/details',async(req, res) => {
+    const id = req.query.id
+    const result = await getDocumentById(id,"Book")
+    const category = await getDocumentById(result.category,"Category")
+    res.render('product_Detail', {details: result, category: category})
+})
 
 
 module.exports = router;
