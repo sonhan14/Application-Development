c5c7a8733b884f1d486fd68198b1dade6e383639
CodeLAB <91421918+DO-HOAI-NAM@users.noreply.github.com>
2022-01-20 19:38:02 +0700

    test

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ const session = require("express-session");
 app.set("view engine", "hbs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(session({
+app.use(
+  session({
     secret: "huong123@@##&&",
     cookie: { maxAge: 1000 * 60 * 60 * 24 },
     saveUninitialized: false,
     resave: false,
-})
+  })
 );
 
 
@@ -23,6 +24,7 @@ app.use("/", customerController);
 //     const user = req.session['user']
 //     res.render('index', {userInfo : user})
 // })
+
 
 const shoppingCart = require("./controllers/cart");
 app.use("/shoppingCart", shoppingCart);
@@ -52,14 +54,106 @@ app.post("/login", async (req, res) => {
         } else {
             res.render('login', { errorMsg: "not auth!!" })
         }
+
+app.get("/", async (req, res) => {
+  const truyen = await dbHandler.searchObjectbyCategory(
+    "Book",
+    "61e570c7ba41b21dee1346b3"
+  );
+  const ITbook = await dbHandler.searchObjectbyCategory(
+    "Book",
+    "61e570ddba41b21dee1346b4"
+  );
+  const searchInput = req.query.search;
+  if (isNaN(Number.parseFloat(searchInput)) == false) {
+    await SearchObject(
+      searchInput,
+      res,
+      truyen,
+      ITbook,
+      dbHandler.searchObjectbyPrice,
+      "Book",
+      Number.parseFloat(searchInput),
+      " VND"
+    );
+  } else {
+    await SearchObject(
+      searchInput,
+      res,
+      truyen,
+      ITbook,
+      dbHandler.searchObjectbyName,
+      "Book",
+      searchInput,
+      ""
+    );
+  }
+});
+async function SearchObject(
+  searchInput,
+  res,
+  truyen,
+  ITbook,
+  dbFunction,
+  collectionName,
+  searchInput,
+  mess
+) {
+  const resultSearch = await dbFunction(collectionName, searchInput);
+  if (searchInput == null) {
+    res.render("index", { truyens: truyen, ITbooks: ITbook });
+  } else {
+    if (resultSearch.length != 0) {
+      res.render("index", { truyens: truyen, ITbooks: ITbook });
+    } else {
+      const message = "Not found " + searchInput + mess;
+      res.render("index", {
+        truyens: truyen,
+        ITbooks: ITbook,
+        errorSearch: message,
+      });
     }
+  }
+}
+
+app.post("/login", async (req, res) => {
+  const name = req.body.txtName;
+  const pass = req.body.txtPass;
+  const role = await dbHandler.checkUserRole(name, pass);
+  console.log(role);
+  if (role == -1) {
+    res.render("login");
+  } else {
+    console.log(role);
+    if (role == -1) {
+      res.render("login", { errorMsg: "Login failed!" });
+    } else {
+      req.session.user = {
+        name: name,
+        role: role,
+      };
+      res.redirect("/");
+
+    }
+  }
 });
 
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+//cac request co chua /admin se di den controller customer
+const userController = require("./controllers/customer");
+app.use("/", userController);
 
 //cac request co chua /admin se di den controller admin
 const adminController = require("./controllers/admin");
 app.use("/admin", adminController);
 
+app.get("/manageCustomerOrder", (req, res) => {
+  res.render("manageCustomerOrder");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
